81abdb7be393481968500bc224b5b6fd60a3bdee
cqumse2006 <cqumse2006@gmail.com>
2025-06-12 11:53:00 +0800

    Delete borderlands4pro-easysite directory

--- borderlands4pro-easysite/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById("root")!).render(<App data-id="9e9gzr4mr" data-path="src/main.tsx" />);
